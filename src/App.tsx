80f9573ab04595d4d02d23b64b992c8eb213d08e
ButkovskiyLev <lebutkovskiy@maximum.auto>
2024-09-25 17:51:00 +0300

    feat: compass component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import {useEffect, useState} from "react"
 import pJson from "../package.json"
 import "./App.css"
 import reactLogo from "./assets/react.svg"
+import {Compass} from "./shared/ui/Compass"
 import viteLogo from "/vite.svg"
 
 function App() {
-	const [deviceOrientation, setDeviceOrientation] = useState<DeviceOrientationEvent>()
 	const [position, setPosition] = useState<GeolocationPosition>()
 	const [permissionState, setPermissionState] = useState<string>()
-	const [isOrientationPermitted, setIsOrientationPermitted] = useState(false)
 
 	useEffect(() => {
 		const watchId = navigator.geolocation.watchPosition(
@@ -25,41 +24,6 @@ function App() {
 		}
 	}, [])
 
-	useEffect(() => {
-		if (!isOrientationPermitted) return
-
-		const handleOrientation = (event: DeviceOrientationEvent) => {
-			setDeviceOrientation(event)
-		}
-
-		window.addEventListener("deviceorientation", handleOrientation, true)
-
-		return () => {
-			window.removeEventListener("deviceorientation", handleOrientation, true)
-		}
-	}, [isOrientationPermitted])
-
-	const handleOrientationAccessClick = async () => {
-		if ((DeviceOrientationEvent as any).requestPermission) {
-			try {
-				const permissionState = await (DeviceOrientationEvent as any).requestPermission()
-				alert(permissionState)
-				if (permissionState === "granted") {
-					alert("granted")
-					setIsOrientationPermitted(true)
-				} else {
-					alert("denied")
-					console.error("Permission denied for device orientation.")
-				}
-			} catch (error) {
-				alert(error)
-				console.error("Error requesting permission for device orientation:", error)
-			}
-		} else {
-			alert("old device")
-		}
-	}
-
 	return (
 		<>
 			<div>
@@ -73,16 +37,12 @@ function App() {
 			<h1>Vite + React</h1>
 			<div className='card'>
 				<div style={{display: "flex", flexDirection: "column", gap: 2}}>
-					<code>
-						Orienatition: {deviceOrientation?.alpha?.toFixed(2)} {deviceOrientation?.beta?.toFixed(2)}{" "}
-						{deviceOrientation?.gamma?.toFixed(2)}
-					</code>
+					<Compass />
 					<code>
 						Position: {position?.coords.latitude} {position?.coords.longitude}
 					</code>
 					<code>Permission: {JSON.stringify(permissionState, null, 2)}</code>
 				</div>
-				<button onClick={handleOrientationAccessClick}>Allow orientation</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
---
+++ src/shared/ui/Compass/Compass.tsx
@@ -0,0 +1,65 @@
+import {useEffect, useState} from "react"
+
+export const Compass = () => {
+	const [deviceOrientation, setDeviceOrientation] = useState<DeviceOrientationEvent>()
+	const [isOrientationPermitted, setIsOrientationPermitted] = useState(false)
+	const [direction, setDirection] = useState<string>()
+
+	useEffect(() => {
+		if (!isOrientationPermitted) return
+
+		const handleOrientation = (event: DeviceOrientationEvent) => {
+			setDeviceOrientation(event)
+
+			if (!event.alpha) return
+
+			if (event.alpha >= 337.5 || event.alpha < 22.5) setDirection("North")
+			else if (event.alpha >= 22.5 && event.alpha < 67.5) setDirection("North-East")
+			else if (event.alpha >= 67.5 && event.alpha < 112.5) setDirection("East")
+			else if (event.alpha >= 112.5 && event.alpha < 157.5) setDirection("South-East")
+			else if (event.alpha >= 157.5 && event.alpha < 202.5) setDirection("South")
+			else if (event.alpha >= 202.5 && event.alpha < 247.5) setDirection("South-West")
+			else if (event.alpha >= 247.5 && event.alpha < 292.5) setDirection("West")
+			else if (event.alpha >= 292.5 && event.alpha < 337.5) setDirection("North-West")
+		}
+
+		window.addEventListener("deviceorientation", handleOrientation, true)
+
+		return () => {
+			window.removeEventListener("deviceorientation", handleOrientation, true)
+		}
+	}, [isOrientationPermitted])
+
+	const handleOrientationAccessClick = async () => {
+		if ((DeviceOrientationEvent as any).requestPermission) {
+			try {
+				const permissionState = await (DeviceOrientationEvent as any).requestPermission()
+				alert(permissionState)
+				if (permissionState === "granted") {
+					alert("granted")
+					setIsOrientationPermitted(true)
+				} else {
+					alert("denied")
+					console.error("Permission denied for device orientation.")
+				}
+			} catch (error) {
+				alert(error)
+				console.error("Error requesting permission for device orientation:", error)
+			}
+		} else {
+			setIsOrientationPermitted(true)
+			alert("old device")
+		}
+	}
+
+	if (!isOrientationPermitted) return <button onClick={handleOrientationAccessClick}>Allow orientation</button>
+
+	return (
+		<div>
+			<code>
+				Orienatition: {direction} {deviceOrientation?.alpha?.toFixed(2)} {deviceOrientation?.beta?.toFixed(2)}{" "}
+				{deviceOrientation?.gamma?.toFixed(2)}
+			</code>
+		</div>
+	)
+}
