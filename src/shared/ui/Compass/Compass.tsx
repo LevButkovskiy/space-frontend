3fb7d28ab996abc9e63c918ecd718686594fa160
ButkovskiyLev <lebutkovskiy@maximum.auto>
2024-09-25 17:56:23 +0300

    feat: compass callibration

--- src/shared/ui/Compass/Compass.tsx
+++ src/shared/ui/Compass/Compass.tsx
@@ -11,16 +11,21 @@ export const Compass = () => {
 		const handleOrientation = (event: DeviceOrientationEvent) => {
 			setDeviceOrientation(event)
 
-			if (!event.alpha) return
+			let {alpha} = event
+			if (!alpha) return
 
-			if (event.alpha >= 337.5 || event.alpha < 22.5) setDirection("North")
-			else if (event.alpha >= 22.5 && event.alpha < 67.5) setDirection("North-East")
-			else if (event.alpha >= 67.5 && event.alpha < 112.5) setDirection("East")
-			else if (event.alpha >= 112.5 && event.alpha < 157.5) setDirection("South-East")
-			else if (event.alpha >= 157.5 && event.alpha < 202.5) setDirection("South")
-			else if (event.alpha >= 202.5 && event.alpha < 247.5) setDirection("South-West")
-			else if (event.alpha >= 247.5 && event.alpha < 292.5) setDirection("West")
-			else if (event.alpha >= 292.5 && event.alpha < 337.5) setDirection("North-West")
+			if (window.screen.orientation && window.screen.orientation.angle === 180) {
+				alpha = (alpha + 180) % 360 // компенсируем 180 градусов
+			}
+
+			if (alpha >= 337.5 || alpha < 22.5) setDirection("North")
+			else if (alpha >= 22.5 && alpha < 67.5) setDirection("North-East")
+			else if (alpha >= 67.5 && alpha < 112.5) setDirection("East")
+			else if (alpha >= 112.5 && alpha < 157.5) setDirection("South-East")
+			else if (alpha >= 157.5 && alpha < 202.5) setDirection("South")
+			else if (alpha >= 202.5 && alpha < 247.5) setDirection("South-West")
+			else if (alpha >= 247.5 && alpha < 292.5) setDirection("West")
+			else if (alpha >= 292.5 && alpha < 337.5) setDirection("North-West")
 		}
 
 		window.addEventListener("deviceorientation", handleOrientation, true)
